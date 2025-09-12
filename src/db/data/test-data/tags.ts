e1de5cc420e1c9c46d94f20cab0f023d841740b2
scottgirling <scottgirling0812@gmail.com>
2025-09-12 17:42:59 +0100

    amend tags test data format

--- src/db/data/test-data/tags.ts
+++ src/db/data/test-data/tags.ts
@@ -4,43 +4,43 @@ export const testTags: Tag[] = [
   {
     "tag_name": "Italian",
     "tag_slug": "italian",
-    "tag_created_at": "2025-09-05T10:00:00Z",
-    "tag_last_updated_at": "2025-09-05T10:00:00Z"
+    "tag_created_at": "2025-09-05T10:00:00.000Z",
+    "tag_last_updated_at": "2025-09-05T10:00:00.000Z"
   },
   {
     "tag_name": "Breakfast",
     "tag_slug": "breakfast",
-    "tag_created_at": "2025-09-05T10:01:00Z",
-    "tag_last_updated_at": "2025-09-05T10:01:00Z"
+    "tag_created_at": "2025-09-05T10:01:00.000Z",
+    "tag_last_updated_at": "2025-09-05T10:01:00.000Z"
   },
   {
     "tag_name": "Thai",
     "tag_slug": "thai",
-    "tag_created_at": "2025-09-05T10:03:00Z",
-    "tag_last_updated_at": "2025-09-05T10:03:00Z"
+    "tag_created_at": "2025-09-05T10:03:00.000Z",
+    "tag_last_updated_at": "2025-09-05T10:03:00.000Z"
   },
   {
     "tag_name": "Curry",
     "tag_slug": "curry",
-    "tag_created_at": "2025-09-05T10:04:00Z",
-    "tag_last_updated_at": "2025-09-05T10:04:00Z"
+    "tag_created_at": "2025-09-05T10:04:00.000Z",
+    "tag_last_updated_at": "2025-09-05T10:04:00.000Z"
   },
   {
     "tag_name": "Vegetarian",
     "tag_slug": "vegetarian",
-    "tag_created_at": "2025-09-05T10:05:00Z",
-    "tag_last_updated_at": "2025-09-05T10:05:00Z"
+    "tag_created_at": "2025-09-05T10:05:00.000Z",
+    "tag_last_updated_at": "2025-09-05T10:05:00.000Z"
   },
   {
     "tag_name": "Quick & Easy",
     "tag_slug": "quick-easy",
-    "tag_created_at": "2025-09-05T10:06:00Z",
-    "tag_last_updated_at": "2025-09-05T10:06:00Z"
+    "tag_created_at": "2025-09-05T10:06:00.000Z",
+    "tag_last_updated_at": "2025-09-05T10:06:00.000Z"
   },
   {
     "tag_name": "Healthy",
     "tag_slug": "healthy",
-    "tag_created_at": "2025-09-05T10:07:00Z",
-    "tag_last_updated_at": "2025-09-05T10:07:00Z"
+    "tag_created_at": "2025-09-05T10:07:00.000Z",
+    "tag_last_updated_at": "2025-09-05T10:07:00.000Z"
   }
 ]
