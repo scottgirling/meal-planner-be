6c3d0c3fc1efbdad261457a3a17561b767737bb6
scottgirling <scottgirling0812@gmail.com>
2025-09-12 18:56:46 +0100

    add GET /api/user_favourite_recipes/:user_id model

---
+++ src/models/selectUserFavouriteRecipes.ts
@@ -0,0 +1,9 @@
+import db from "../db/connection.js";
+import { Recipe } from "../types/recipe.js";
+
+export const selectUserFavouriteRecipes = (user_id: string) => {
+    return db.query("SELECT recipes.* FROM recipes JOIN user_favourite_recipes ON recipes.recipe_id = user_favourite_recipes.recipe_id WHERE user_favourite_recipes.user_id = $1", [user_id])
+    .then(({ rows } : { rows: Recipe[] }) => {
+        return rows;
+    });
+}
