10ba13131b6ef7da2c2f20b994c47d2377c899fb
scottgirling <scottgirling0812@gmail.com>
2025-09-12 17:40:53 +0100

    add GET /api/recipes/:recipe_id/tags model

---
+++ src/models/selectTagsByRecipeId.ts
@@ -0,0 +1,12 @@
+import db from "../db/connection.js";
+import { Tag } from "../types/tag.js";
+
+export const selectTagsByRecipeId = (recipe_id: string) => {
+    return db.query("SELECT tags.* FROM recipe_tags JOIN tags ON recipe_tags.tag_id = tags.tag_id WHERE recipe_tags.recipe_id = $1", [recipe_id])
+    .then(({ rows } : { rows: Tag[] }) => {
+        if (!rows.length) {
+            return Promise.reject({ status: 404, msg: "Recipe does not exist." });
+        }
+        return rows;
+    });
+}
