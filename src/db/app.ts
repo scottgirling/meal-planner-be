6439a5b6949feed1b9ca9b6d205a90f89aff2136
scottgirling <scottgirling0812@gmail.com>
2025-09-13 19:17:31 +0100

    add getUserMealPlans controller to app

--- src/db/app.ts
+++ src/db/app.ts
@@ -9,22 +9,24 @@ export const app = express();
 import { getEndpoints } from "../controllers/getEndpoints";
 import { getTags } from "../controllers/getTags";
 import { getRecipes } from "../controllers/getRecipes";
-import { getRecipeById } from "../controllers/getRecipeById";
-import { getIngredientsByRecipeId } from "../controllers/getIngredientsByRecipeId";
-import { getTagsByRecipeId } from "../controllers/getTagsByRecipeId";
-import { getUserById } from "../controllers/getUserById";
+import { getRecipe } from "../controllers/getRecipe";
+import { getRecipeIngredients } from "../controllers/getRecipeIngredients";
+import { getRecipeTags } from "../controllers/getRecipeTags";
+import { getUser } from "../controllers/getUser";
 import { getUserFavouriteRecipes } from "../controllers/getUserFavouriteRecipes";
+import { getUserMealPlans } from "../controllers/getUserMealPlans";
 
 app.use(cors());
 
 app.get("/api", getEndpoints);
 app.get("/api/tags", getTags);
 app.get("/api/recipes", getRecipes);
-app.get("/api/recipes/:recipe_id", getRecipeById);
-app.get("/api/recipes/:recipe_id/ingredients", getIngredientsByRecipeId);
-app.get("/api/recipes/:recipe_id/tags", getTagsByRecipeId);
-app.get("/api/users/:user_id", getUserById);
-app.get("/api/user_favourite_recipes/:user_id", getUserFavouriteRecipes);
+app.get("/api/recipes/:recipe_id", getRecipe);
+app.get("/api/recipes/:recipe_id/ingredients", getRecipeIngredients);
+app.get("/api/recipes/:recipe_id/tags", getRecipeTags);
+app.get("/api/users/:user_id", getUser);
+app.get("/api/users/:user_id/favourite_recipes", getUserFavouriteRecipes);
+app.get("/api/users/:user_id/meal_plans", getUserMealPlans);
 
 app.use((error: CustomError, request: Request, response: Response, next: NextFunction) => {
     if (error.status && error.msg) {
