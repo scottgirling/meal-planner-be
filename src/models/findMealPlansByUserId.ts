edcd872c07c71c1ddbca973b8a12522626276e7e
scottgirling <scottgirling0812@gmail.com>
2025-09-14 18:07:45 +0100

    add GET /api/users/:user_id/shopping_list model

--- src/models/findMealPlansByUserId.ts
+++ src/models/findMealPlansByUserId.ts
@@ -5,5 +5,5 @@ export const findMealPlansByUserId = (user_id: string) => {
     return db.query("SELECT meal_plan_recipes.meal_plan_id, meal_plan_recipes.scheduled_date::text, recipes.* FROM meal_plans JOIN meal_plan_recipes ON meal_plans.meal_plan_id = meal_plan_recipes.meal_plan_id JOIN recipes ON meal_plan_recipes.recipe_id = recipes.recipe_id WHERE meal_plans.meal_plan_created_by = $1", [user_id])
     .then(({ rows } : { rows: UserMealPlanRecipe[] }) => {
         return rows;
-    })
+    });
 }
