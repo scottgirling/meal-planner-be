f6f09ccb4356739021d526e27cdcf1fe14815134
scottgirling <scottgirling0812@gmail.com>
2025-09-20 10:31:48 +0100

    add error handling to POST /api/users/:user_id/shopping_lists controller

--- src/controllers/postUserShoppingList.ts
+++ src/controllers/postUserShoppingList.ts
@@ -4,6 +4,8 @@ import db from "../db/connection.js";
 import { createUserShoppingList } from "../models/createUserShoppingList";
 import { findIngredientsForShoppingList } from "../models/findIngredientsForShoppingList.js";
 import { createShoppingListIngredient } from "../models/createShoppingListIngredient.js";
+import { checkUserExists } from "../utils/checkUserExists.js";
+import { checkRecipeExists } from "../utils/checkRecipeExists.js";
 
 export const postUserShoppingList = async (request: Request, response: Response, next: NextFunction) => {
     const { user_id } = request.params;
@@ -12,7 +14,30 @@ export const postUserShoppingList = async (request: Request, response: Response,
         recipe_ids
     } = request.body;
 
+    if (
+        meal_plan_id === undefined ||
+        recipe_ids === undefined
+    ) {
+        return Promise.reject({ status: 400, msg: "Invalid request - missing field(s)." });
+    }
+
+    if (!Array.isArray(recipe_ids)) {
+        return Promise.reject({ status: 400, msg: "Invalid data type." });
+    }
+
     let newShoppingListId: (number | undefined) = 0;
+
+    checkUserExists(user_id)
+    .catch((error) => {
+        next(error);
+    });
+
+    recipe_ids.forEach((recipe_id) => {
+        checkRecipeExists(recipe_id)
+        .catch((error) => {
+            next(error);
+        });
+    });
 
     let client: PoolClient | undefined;
 
