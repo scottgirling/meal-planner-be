c6388745ee43e6d4329a7ff0c99d1c28014a0d07
scottgirling <scottgirling0812@gmail.com>
2025-09-20 11:46:17 +0100

    add checkRecipeIsPublic util func

--- src/utils/checkRecipeExists.ts
+++ src/utils/checkRecipeExists.ts
@@ -1,7 +1,7 @@
 import db from "../db/connection.js";
 import { Recipe } from "../types/recipe.js";
 
-export const checkRecipeExists = (recipe_id: number) => {
+export const checkRecipeExists = (recipe_id: string) => {
     return db.query("SELECT * FROM recipes WHERE recipe_id = $1", [recipe_id])
     .then(({ rows } : { rows: Recipe[] }) => {
         if (!rows.length) {
