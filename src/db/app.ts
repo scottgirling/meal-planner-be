21e4d4a64b16f0fcaa115074c318f37ad1b08fd2
scottgirling <scottgirling0812@gmail.com>
2025-09-22 19:25:22 +0100

    add deleteUserMealPlan to app

--- src/db/app.ts
+++ src/db/app.ts
@@ -28,6 +28,7 @@ import { deleteRecipe } from "../controllers/deleteRecipe";
 import { deleteUser } from "../controllers/deleteUser";
 import { deleteUserFavouriteRecipe } from "../controllers/deleteUserFavouriteRecipe";
 import { deleteUserShoppingList } from "../controllers/deleteUserShoppingList";
+import { deleteUserMealPlan } from "../controllers/deleteUserMealPlan";
 
 app.use(cors());
 app.use(express.json());
@@ -54,6 +55,7 @@ app.delete("/api/recipes/:recipe_id", deleteRecipe);
 app.delete("/api/users/:user_id", deleteUser);
 app.delete("/api/users/:user_id/favourite_recipes/:recipe_id", deleteUserFavouriteRecipe);
 app.delete("/api/users/:user_id/shopping_lists/:shopping_list_id", deleteUserShoppingList);
+app.delete("/api/users/:user_id/meal_plans/:meal_plan_id", deleteUserMealPlan);
 
 app.use((error: CustomError, request: Request, response: Response, next: NextFunction) => {
     if (error.status && error.msg) {
