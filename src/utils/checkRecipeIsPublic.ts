4dfd0063c14c54c458ad6535b36a90f41cf27fd9
scottgirling <scottgirling0812@gmail.com>
2025-09-24 19:58:38 +0100

    add optional DBClient to util funcs to handle transactions

--- src/utils/checkRecipeIsPublic.ts
+++ src/utils/checkRecipeIsPublic.ts
@@ -1,11 +1,12 @@
+import { DBClient } from "../types/db-client.js";
 import db from "../db/connection.js";
 import { Recipe } from "../types/recipe.js";
 
-export const checkRecipeIsPublic = (recipe_id: string) => {
-    return db.query("SELECT * FROM recipes WHERE is_recipe_public = 'false' AND recipe_id = $1", [recipe_id])
+export const checkRecipeIsPublic = (recipe_id: string, client: DBClient = db) => {
+    return db.query("SELECT * FROM recipes WHERE is_recipe_public = false AND recipe_id = $1", [recipe_id])
     .then(({ rows } : { rows: Recipe[] }) => {
         if (!rows.length) {
-            return Promise.reject({ status: 400, msg: "Invalid request - public recipes cannot be removed." });
+            return Promise.reject({ status: 403, msg: "Forbidden request - unable to amend/remove public recipes." });
         }
     });
 }
