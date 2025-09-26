70189c5d8fe7356939adb47919f2717b01b091ce
scottgirling <scottgirling0812@gmail.com>
2025-09-26 16:55:20 +0100

    add error handling to PATCH /api/users/:user_id controller

--- src/controllers/patchUser.ts
+++ src/controllers/patchUser.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { checkUserExists } from "../utils/checkUserExists";
 import { updateUserById } from "../models/updateUserById";
 
 export const patchUser = async (request: Request, response: Response, next: NextFunction) => {
@@ -11,12 +12,20 @@ export const patchUser = async (request: Request, response: Response, next: Next
         avatar_url
     } = request.body;
 
+    if (!Object.entries(request.body).length) {
+        throw {
+            status: 400,
+            msg: "Invalid request - missing field(s)."
+        }
+    }
+
     try {
+        await checkUserExists(user_id);
         const user = await updateUserById(user_id, user_name, username, email, bio, avatar_url);
 
         return response.status(200).send({ user });
         
     } catch (error) {
-
+        next(error);
     }
 }
