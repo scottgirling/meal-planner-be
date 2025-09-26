b50747ec98180f416b8f4afaa20144512636e1b2
scottgirling <scottgirling0812@gmail.com>
2025-09-26 19:10:04 +0100

    refactor to async/await, custom error and additional TS safety

--- src/utils/checkRecipeExists.ts
+++ src/utils/checkRecipeExists.ts
@@ -1,12 +1,21 @@
 import { DBClient } from "../types/db-client.js";
 import db from "../db/connection.js";
 import { Recipe } from "../types/recipe.js";
+import { NotFoundError } from "../types/errors.js";
 
-export const checkRecipeExists = (recipe_id: string, client: DBClient = db) => {
-    return client.query("SELECT * FROM recipes WHERE recipe_id = $1", [recipe_id])
-    .then(({ rows } : { rows: Recipe[] }) => {
-        if (!rows.length) {
-            return Promise.reject({ status: 404, msg: "Recipe does not exist." });
-        }
-    });
+export const checkRecipeExists = async (recipe_id: string, client: DBClient = db): Promise<Recipe> => {
+
+    const result = await client.query<Recipe>(`
+        SELECT *
+        FROM recipes
+        WHERE recipe_id = $1
+        `, [recipe_id]
+    );
+    const [recipe] = result.rows;
+
+    if (!recipe) {
+        throw new NotFoundError("Recipe does not exist.");
+    }
+
+    return recipe;
 }
