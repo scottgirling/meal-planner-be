80b5ace9cec82d10ef5211a3fee8d33f870e7e8d
scottgirling <scottgirling0812@gmail.com>
2025-09-26 19:08:59 +0100

    refactor to async/await and additional TS safety

--- src/models/findRecipeById.ts
+++ src/models/findRecipeById.ts
@@ -1,12 +1,22 @@
 import db from "../db/connection.js";
 import { AdditionalRecipeInfo } from "../types/additional-recipe-info.js";
+import { NotFoundError } from "../types/errors.js";
 
-export const findRecipeById = (recipe_id: string) => {
-    return db.query("SELECT recipes.*, users.username, users.avatar_url FROM recipes JOIN users ON recipes.recipe_created_by = users.user_id WHERE recipes.recipe_id = $1", [recipe_id])
-    .then(({ rows } : { rows: AdditionalRecipeInfo[] }) => {
-        if (!rows.length) {
-            return Promise.reject({ status: 404, msg: "Recipe does not exist." });
-        }
-        return rows[0];
-    });
+export const findRecipeById = async (recipe_id: string): Promise<AdditionalRecipeInfo> => {
+    
+    const result = await db.query<AdditionalRecipeInfo>(`
+        SELECT recipes.*, users.username, users.avatar_url 
+        FROM recipes 
+        JOIN users 
+        ON recipes.recipe_created_by = users.user_id 
+        WHERE recipes.recipe_id = $1
+        `, [recipe_id]
+    );
+    const recipe = result.rows[0];
+
+    if (!recipe) {
+        throw new NotFoundError("Recipe does not exist.");
+    }
+
+    return recipe;
 }
