f1cce310cbd5bbe6e7637c9f450780889a3d7341
scottgirling <scottgirling0812@gmail.com>
2025-09-29 13:49:51 +0100

    refactor to async/await try/catch

--- src/controllers/getTags.ts
+++ src/controllers/getTags.ts
@@ -2,9 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import { findAllTags } from "../models/findAllTags.js";
 import { Tag } from "../types/tag.js";
 
-export const getTags = (request: Request, response: Response, next: NextFunction) => {
-    findAllTags()
-    .then((tags: Tag[]) => {
+export const getTags = async (request: Request, response: Response, next: NextFunction) => {
+
+    try {
+        const tags: Tag[] = await findAllTags();
+
         response.status(200).send({ tags });
-    });
+    } catch (error: unknown) {
+        next(error);
+    }
 }
