690f3f46e211a6990b87041fcffa36d6e2244df4
scottgirling <scottgirling0812@gmail.com>
2025-09-29 14:10:52 +0100

    refactor to async/await

--- src/models/findFavouriteRecipesByUserId.ts
+++ src/models/findFavouriteRecipesByUserId.ts
@@ -1,9 +1,18 @@
+import { DBClient } from "../types/db-client.js";
 import db from "../db/connection.js";
 import { Recipe } from "../types/recipe.js";
 
-export const findFavouriteRecipesByUserId = (user_id: string) => {
-    return db.query("SELECT recipes.* FROM recipes JOIN user_favourite_recipes ON recipes.recipe_id = user_favourite_recipes.recipe_id WHERE user_favourite_recipes.user_id = $1", [user_id])
-    .then(({ rows } : { rows: Recipe[] }) => {
-        return rows;
-    });
+export const findFavouriteRecipesByUserId = async (user_id: string, client: DBClient = db): Promise<Recipe[]> => {
+
+    const result = await client.query<Recipe>(`
+        SELECT recipes.* 
+        FROM recipes 
+        JOIN user_favourite_recipes 
+        ON recipes.recipe_id = user_favourite_recipes.recipe_id 
+        WHERE user_favourite_recipes.user_id = $1
+        `, [user_id]
+    );
+    const recipes = result.rows;
+
+    return recipes;
 }
