6bc496535042de896f7b157cc0a6f30106333914
scottgirling <scottgirling0812@gmail.com>
2025-09-30 16:37:14 +0100

    refactor to async/await try/catch

--- src/controllers/postRecipe.ts
+++ src/controllers/postRecipe.ts
@@ -1,15 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { RecipeBody } from "../types/req-body/RecipeBody";
 import { createRecipe } from "../models/createRecipe";
 import { Recipe } from "../types";
 
-export const postRecipe = (request: Request, response: Response, next: NextFunction) => {
+export const postRecipe = async (
+    request: Request<{}, {}, RecipeBody>, 
+    response: Response, 
+    next: NextFunction
+) => {
     const { 
         recipe_name, 
         recipe_slug, 
         instructions, 
         prep_time, 
         cook_time, 
-        votes, 
         servings, 
         recipe_created_by, 
         recipe_img_url, 
@@ -17,23 +21,22 @@ export const postRecipe = (request: Request, response: Response, next: NextFunct
         is_recipe_public 
     } = request.body;
 
-    createRecipe(
-        recipe_name, 
-        recipe_slug, 
-        instructions, 
-        prep_time, 
-        cook_time, 
-        votes, 
-        servings, 
-        recipe_created_by, 
-        recipe_img_url, 
-        difficulty, 
-        is_recipe_public
-    )
-    .then((recipe: Recipe) => {
-        return response.status(201).send({ recipe });
-    })
-    .catch((error: Error) => {
+    try {
+        const recipe: Recipe = await createRecipe(
+            recipe_name, 
+            recipe_slug, 
+            instructions, 
+            prep_time, 
+            cook_time, 
+            servings, 
+            recipe_created_by, 
+            recipe_img_url, 
+            difficulty, 
+            is_recipe_public
+        );
+
+        response.status(201).send({ recipe });
+    } catch (error: unknown) {
         next(error);
-    });
+    }
 }
