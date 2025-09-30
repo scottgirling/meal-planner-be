eff565fae435d7eabe90fc8dddfc555b67335903
scottgirling <scottgirling0812@gmail.com>
2025-09-30 18:02:51 +0100

    add generic for req.param

--- src/controllers/deleteUserShoppingList.ts
+++ src/controllers/deleteUserShoppingList.ts
@@ -3,7 +3,11 @@ import { checkUserExists } from "../utils/checkUserExists";
 import { checkShoppingListExists } from "../utils/checkShoppingListExists";
 import { removeUserShoppingList } from "../models/removeUserShoppingList";
 
-export const deleteUserShoppingList = async (request: Request, response: Response, next: NextFunction) => {
+export const deleteUserShoppingList = async (
+    request: Request<{ user_id: string, shopping_list_id: string }>, 
+    response: Response, 
+    next: NextFunction
+) => {
     const {
         user_id, 
         shopping_list_id
