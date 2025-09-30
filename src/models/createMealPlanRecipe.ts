c8e1201201eb7c116dd597144d95ef40443e7eb5
scottgirling <scottgirling0812@gmail.com>
2025-09-30 17:53:53 +0100

    refactor to async/await and add DBClient

--- src/models/createMealPlanRecipe.ts
+++ src/models/createMealPlanRecipe.ts
@@ -1,7 +1,13 @@
-import { PoolClient } from "pg";
+import { DBClient } from "../types/db-client.js";
+import db from "../db/connection.js";
 import { MealPlanRecipe } from "../types/meal-plan-recipe.js";
 
-export const createMealPlanRecipe = (client: PoolClient, newMealPlanId: number | undefined, recipe_ids: number[], scheduled_dates: string[]) => {
+export const createMealPlanRecipe = async (
+    newMealPlanId: number | undefined, 
+    recipe_ids: string[], 
+    scheduled_dates: string[],
+    client: DBClient = db, 
+): Promise<MealPlanRecipe[]> => {
 
     const positionalPlaceholders: string[] = [];
     const queryParams: (string | number)[] = [];
@@ -12,8 +18,13 @@ export const createMealPlanRecipe = (client: PoolClient, newMealPlanId: number |
         queryParams.push(Number(newMealPlanId), recipe_id, scheduled_dates[index]);
     });
 
-    return client.query(`INSERT INTO meal_plan_recipes (meal_plan_id, recipe_id, scheduled_date) VALUES ${positionalPlaceholders.join(", ")} RETURNING *`, queryParams)
-    .then(({ rows } : { rows: MealPlanRecipe[] }) => {
-        return rows;
-    });
+    const result = await client.query<MealPlanRecipe>(`
+        INSERT INTO meal_plan_recipes (meal_plan_id, recipe_id, scheduled_date) 
+        VALUES ${positionalPlaceholders.join(", ")} 
+        RETURNING *
+        `, queryParams
+    );
+    const meal_plan_recipes = result.rows;
+
+    return meal_plan_recipes;
 }
