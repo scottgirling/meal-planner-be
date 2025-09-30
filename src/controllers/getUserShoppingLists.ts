ce4e0dd049a7f7563e33e8e5d680e2c3bcdef43a
scottgirling <scottgirling0812@gmail.com>
2025-09-30 16:10:54 +0100

    refactor to async/await and try/catch

--- src/controllers/getUserShoppingLists.ts
+++ src/controllers/getUserShoppingLists.ts
@@ -3,19 +3,33 @@ import { findShoppingListsByUserId } from "../models/findShoppingListsByUserId";
 import { ShoppingListIngredient } from "../types";
 import { checkUserExists } from "../utils/checkUserExists";
 
-export const getUserShoppingLists = (request: Request, response: Response, next: NextFunction) => {
+export const getUserShoppingLists = async (
+    request: Request<{ user_id: string }>, 
+    response: Response, 
+    next: NextFunction
+) => {
     const { user_id } = request.params;
 
-    checkUserExists(user_id)
-    .catch((error: Error) => {
-        next(error);
-    });
+    try {
+        await checkUserExists(user_id);
 
-    findShoppingListsByUserId(user_id)
-    .then((items: ShoppingListIngredient[]) => {
-        return response.status(200).send({ items });
-    })
-    .catch((error: Error) => {
+        const items: ShoppingListIngredient[] = await findShoppingListsByUserId(user_id);
+
+        response.status(200).send({ items });
+    } catch (error: unknown) {
         next(error);
-    });
+    }
+
+    // checkUserExists(user_id)
+    // .catch((error: Error) => {
+    //     next(error);
+    // });
+
+    // findShoppingListsByUserId(user_id)
+    // .then((items: ShoppingListIngredient[]) => {
+    //     return response.status(200).send({ items });
+    // })
+    // .catch((error: Error) => {
+    //     next(error);
+    // });
 }
