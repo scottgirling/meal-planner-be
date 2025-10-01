31e3dc414c0379014f432f89074208b3511887fa
scottgirling <scottgirling0812@gmail.com>
2025-10-01 13:37:51 +0100

    refactor to async/await and add custom error

--- src/utils/checkMealPlanExists.ts
+++ src/utils/checkMealPlanExists.ts
@@ -1,11 +1,24 @@
+import { DBClient } from "../types/db-client.js";
 import db from "../db/connection.js";
 import { MealPlan } from "../types/meal-plan.js";
+import { NotFoundError } from "../types/errors.js";
 
-export const checkMealPlanExists = (meal_plan_id: string) => {
-    return db.query("SELECT * FROM meal_plans WHERE meal_plan_id = $1", [meal_plan_id])
-    .then(({ rows } : { rows: MealPlan[] }) => {
-        if (!rows.length) {
-            return Promise.reject({ status: 404, msg: "Meal Plan does not exist." });
-        }
-    });
+export const checkMealPlanExists = async (
+    meal_plan_id: string,
+    client: DBClient = db
+): Promise<MealPlan> => {
+    
+    const result = await client.query<MealPlan>(`
+        SELECT * 
+        FROM meal_plans 
+        WHERE meal_plan_id = $1
+        `, [meal_plan_id]
+    );
+    const [mealPlan] = result.rows;
+
+    if (!mealPlan) {
+        throw new NotFoundError("Meal Plan does not exist.");
+    }
+
+    return mealPlan;
 }
