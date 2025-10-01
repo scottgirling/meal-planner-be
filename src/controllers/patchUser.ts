50b69bb9acc717f3166e50d9c8041759a20f82ac
scottgirling <scottgirling0812@gmail.com>
2025-10-01 14:11:25 +0100

    add PatchUserBody generic and custom error

--- src/controllers/patchUser.ts
+++ src/controllers/patchUser.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import { PatchUserBody } from "../types/req-body/PatchUserBody";
+import { InvalidRequestError } from "../types/errors";
 import { checkUserExists } from "../utils/checkUserExists";
 import { updateUserById } from "../models/updateUserById";
 
-export const patchUser = async (request: Request, response: Response, next: NextFunction) => {
+export const patchUser = async (
+    request: Request<{ user_id: string }, {}, PatchUserBody>, 
+    response: Response, 
+    next: NextFunction
+) => {
     const { user_id } = request.params;
     const {
         user_name,
@@ -13,17 +19,21 @@ export const patchUser = async (request: Request, response: Response, next: Next
     } = request.body;
 
     if (!Object.entries(request.body).length) {
-        throw {
-            status: 400,
-            msg: "Invalid request - missing field(s)."
-        }
+        throw new InvalidRequestError("Invalid request - missing field(s).");
     }
 
     try {
         await checkUserExists(user_id);
-        const user = await updateUserById(user_id, user_name, username, email, bio, avatar_url);
+        const user = await updateUserById(
+            user_id, 
+            user_name, 
+            username, 
+            email, 
+            bio, 
+            avatar_url
+        );
 
-        return response.status(200).send({ user });
+        response.status(200).send({ user });
         
     } catch (error) {
         next(error);
