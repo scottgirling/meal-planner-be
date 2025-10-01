68cc7901c98bb02c3214055640cdae18bfd81b1d
scottgirling <scottgirling0812@gmail.com>
2025-10-01 13:11:56 +0100

    refactor util func calls to async and move into try/catch

--- src/controllers/postUserShoppingList.ts
+++ src/controllers/postUserShoppingList.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from "express";
+import { UserShoppingListBody } from "../types/req-body/UserShoppingListBody.js";
 import { PoolClient } from "pg";
 import db from "../db/connection.js";
+import { checkUserExists } from "../utils/checkUserExists.js";
+import { checkRecipeExists } from "../utils/checkRecipeExists.js";
 import { createUserShoppingList } from "../models/createUserShoppingList";
 import { findIngredientsForShoppingList } from "../models/findIngredientsForShoppingList.js";
 import { createShoppingListIngredient } from "../models/createShoppingListIngredient.js";
-import { checkUserExists } from "../utils/checkUserExists.js";
-import { checkRecipeExists } from "../utils/checkRecipeExists.js";
+import { ShoppingList } from "../types/shopping-list.js";
+import { RecipeIngredient } from "../types/recipe-ingredient.js";
+import { ShoppingListIngredient } from "../types/shopping-list-ingredient.js";
 
-export const postUserShoppingList = async (request: Request, response: Response, next: NextFunction) => {
+export const postUserShoppingList = async (
+    request: Request<{ user_id: string }, {}, UserShoppingListBody>, 
+    response: Response, 
+    next: NextFunction
+) => {
     const { user_id } = request.params;
     const { 
         meal_plan_id, 
@@ -27,30 +35,21 @@ export const postUserShoppingList = async (request: Request, response: Response,
 
     let newShoppingListId: (number | undefined) = 0;
 
-    checkUserExists(user_id)
-    .catch((error) => {
-        next(error);
-    });
-
-    recipe_ids.forEach((recipe_id) => {
-        checkRecipeExists(recipe_id)
-        .catch((error) => {
-            next(error);
-        });
-    });
-
     let client: PoolClient | undefined;
 
     try {
+        await checkUserExists(user_id);
+        await Promise.all(recipe_ids.map(recipe => checkRecipeExists(recipe)));
+
         client = await db.connect();
         client.query("BEGIN");
 
-        const shopping_list = await createUserShoppingList(client, user_id, meal_plan_id);
+        const shopping_list: ShoppingList = await createUserShoppingList(user_id, meal_plan_id, client);
         newShoppingListId = shopping_list.shopping_list_id;
 
-        const ingredients = await findIngredientsForShoppingList(client, recipe_ids);
+        const ingredients: RecipeIngredient[] = await findIngredientsForShoppingList(recipe_ids, client);
 
-        const shopping_list_ingredients = await createShoppingListIngredient(client, newShoppingListId, ingredients);
+        const shopping_list_ingredients: ShoppingListIngredient[] = await createShoppingListIngredient(newShoppingListId, ingredients, client);
 
         await client.query("COMMIT");
 
