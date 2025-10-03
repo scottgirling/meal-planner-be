7f08202efdcdf0074441c5515948404254c71424
scottgirling <scottgirling0812@gmail.com>
2025-10-03 13:06:34 +0100

    remove try/catch

--- src/models/updateUserById.ts
+++ src/models/updateUserById.ts
@@ -2,46 +2,49 @@ import { DBClient } from "../types/db-client.js";
 import db from "../db/connection.js";
 import { User } from "@supabase/supabase-js";
 
-export const updateUserById = async (user_id: string, user_name: string, username: string, email: string, bio: string, avatar_url: string, client: DBClient = db) => {
-    
-    try {
-        let queryValues = [];
+export const updateUserById = async (
+    user_id: string, 
+    user_name: string, 
+    username: string, 
+    email: string, 
+    bio: string, 
+    avatar_url: string, 
+    client: DBClient = db
+): Promise<User> => {
+    let queryValues = [];
 
-        let sqlQuery = "UPDATE users SET user_last_updated_at = CURRENT_TIMESTAMP";
+    let sqlQuery = "UPDATE users SET user_last_updated_at = CURRENT_TIMESTAMP";
 
-        if (user_name) {
-            queryValues.push(user_name);
-            sqlQuery += `, user_name = $${queryValues.length}`;
-        }
-
-        if (username) {
-            queryValues.push(username);
-            sqlQuery += `, username = $${queryValues.length}`;
-        }
-
-        if (email) {
-            queryValues.push(email);
-            sqlQuery += `, email = $${queryValues.length}`;
-        }
-
-        if (bio) {
-            queryValues.push(bio);
-            sqlQuery += `, bio = $${queryValues.length}`;
-        }
-
-        if (avatar_url) {
-            queryValues.push(avatar_url);
-            sqlQuery += `, avatar_url = $${queryValues.length}`;
-        }
-
-        queryValues.push(user_id);
-        sqlQuery += ` WHERE user_id = $${queryValues.length} RETURNING *`;
-
-        const result: { rows: User[] } = await client.query(sqlQuery, queryValues);
-        const { rows } = result;
-
-        return rows[0];
-    } catch (error) {
-        throw error;
+    if (user_name) {
+        queryValues.push(user_name);
+        sqlQuery += `, user_name = $${queryValues.length}`;
     }
+
+    if (username) {
+        queryValues.push(username);
+        sqlQuery += `, username = $${queryValues.length}`;
+    }
+
+    if (email) {
+        queryValues.push(email);
+        sqlQuery += `, email = $${queryValues.length}`;
+    }
+
+    if (bio) {
+        queryValues.push(bio);
+        sqlQuery += `, bio = $${queryValues.length}`;
+    }
+
+    if (avatar_url) {
+        queryValues.push(avatar_url);
+        sqlQuery += `, avatar_url = $${queryValues.length}`;
+    }
+
+    queryValues.push(user_id);
+    sqlQuery += ` WHERE user_id = $${queryValues.length} RETURNING *`;
+
+    const result = await client.query<User>(sqlQuery, queryValues);
+    const [updatedUser] = result.rows;
+
+    return updatedUser;
 }
