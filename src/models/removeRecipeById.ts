79a8122386884556a208f7699622cf11bacc4634
scottgirling <scottgirling0812@gmail.com>
2025-10-03 12:59:52 +0100

    refactor to async/await and add DBClient

--- src/models/removeRecipeById.ts
+++ src/models/removeRecipeById.ts
@@ -1,9 +1,19 @@
+import { DBClient } from "../types/db-client.js";
 import db from "../db/connection.js";
 import { Recipe } from "../types/recipe.js";
 
-export const removeRecipeById = (recipe_id: string) => {
-    return db.query("DELETE FROM recipes WHERE recipe_id = $1 RETURNING *", [recipe_id])
-    .then(({ rows } : { rows: Recipe[] }) => {
-        return rows[0];
-    });
+export const removeRecipeById = async (
+    recipe_id: string,
+    client: DBClient = db
+): Promise<Recipe> => {
+
+    const result = await client.query<Recipe>(`
+        DELETE FROM recipes 
+        WHERE recipe_id = $1 
+        RETURNING *
+        `, [recipe_id]
+    );
+    const [removedRecipe] = result.rows;
+
+    return removedRecipe;
 }
