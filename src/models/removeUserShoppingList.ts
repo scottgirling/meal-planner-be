b58fdab26475174f2df62ba5ac16b213c2447bf3
scottgirling <scottgirling0812@gmail.com>
2025-10-03 13:00:43 +0100

    refactor to async/await and add DBClient

--- src/models/removeUserShoppingList.ts
+++ src/models/removeUserShoppingList.ts
@@ -1,9 +1,21 @@
 import db from "../db/connection.js";
+import { DBClient } from "../types/db-client.js";
 import { ShoppingList } from "../types/shopping-list.js";
 
-export const removeUserShoppingList = (user_id: string, shopping_list_id: string) => {
-    return db.query("DELETE FROM shopping_lists WHERE shopping_list_created_by = $1 AND shopping_list_id = $2 RETURNING *", [user_id, shopping_list_id])
-    .then(({ rows } : { rows: ShoppingList[] }) => {
-        return rows[0];
-    });
+export const removeUserShoppingList = async (
+    user_id: string, 
+    shopping_list_id: string,
+    client: DBClient = db
+): Promise<ShoppingList> => {
+
+    const result = await client.query<ShoppingList>(`
+        DELETE FROM shopping_lists 
+        WHERE shopping_list_created_by = $1 
+        AND shopping_list_id = $2 
+        RETURNING *
+        `, [user_id, shopping_list_id]
+    );
+    const [removedShoppingList] = result.rows;
+
+    return removedShoppingList;
 }
