33f32add2d39553df4e8e2117c613171a24be5e2
scottgirling <scottgirling0812@gmail.com>
2025-10-03 13:04:16 +0100

    refactor to async/await and add DBClient

--- src/models/removeUserFavouriteRecipe.ts
+++ src/models/removeUserFavouriteRecipe.ts
@@ -1,9 +1,21 @@
+import { DBClient } from "../types/db-client.js";
 import db from "../db/connection.js";
 import { UserFavouriteRecipe } from "../types/user-favourite-recipe.js";
 
-export const removeUserFavouriteRecipe = (user_id: string, recipe_id: string) => {
-    return db.query("DELETE FROM user_favourite_recipes WHERE user_id = $1 AND recipe_id = $2 RETURNING *", [user_id, recipe_id])
-    .then(({ rows } : { rows: UserFavouriteRecipe[] }) => {
-        return rows[0];
-    });
+export const removeUserFavouriteRecipe = async (
+    user_id: string, 
+    recipe_id: string,
+    client: DBClient = db
+): Promise<UserFavouriteRecipe> => {
+
+    const result = await client.query<UserFavouriteRecipe>(`
+        DELETE FROM user_favourite_recipes 
+        WHERE user_id = $1 
+        AND recipe_id = $2 
+        RETURNING *
+        `, [user_id, recipe_id]
+    );
+    const [removedFavouriteRecipe] = result.rows;
+
+    return removedFavouriteRecipe;
 }
