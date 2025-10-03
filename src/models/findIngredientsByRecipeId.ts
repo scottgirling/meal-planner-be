8ebb73068fba31cfdd309336caf6dc7fc9d6bb8d
scottgirling <scottgirling0812@gmail.com>
2025-10-03 13:12:28 +0100

    add DBClient

--- src/models/findIngredientsByRecipeId.ts
+++ src/models/findIngredientsByRecipeId.ts
@@ -1,10 +1,14 @@
+import { DBClient } from "../types/db-client.js";
 import db from "../db/connection.js";
 import { RecipeIngredient } from "../types/recipe-ingredient.js";
 import { NotFoundError } from "../types/errors.js";
 
-export const findIngredientsByRecipeId = async (recipe_id: string): Promise<RecipeIngredient[]> => {
+export const findIngredientsByRecipeId = async (
+    recipe_id: string,
+    client: DBClient = db
+): Promise<RecipeIngredient[]> => {
 
-    const result = await db.query<RecipeIngredient>(`
+    const result = await client.query<RecipeIngredient>(`
         SELECT ingredients.ingredient_name, recipe_ingredients.quantity, recipe_ingredients.unit FROM recipe_ingredients 
         JOIN ingredients 
         ON recipe_ingredients.ingredient_id = ingredients.ingredient_id 
